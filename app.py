c46c410bac6a7ef68dee0b0a256607801f70410c
R Shupe <shipshupe@users.noreply.github.com>
2024-01-14 23:16:11 +0000

    Updated relationship definition to support nested objects
    Co-authored-by: Garth Johnson <github@thenetyeti.com>

--- app.py
+++ app.py
@@ -1,25 +1,47 @@
+from datetime import datetime
+from decimal import Decimal
 from pydantic import BaseModel
-from sqlmodel import Field, SQLModel, Session, select, create_engine
+from sqlalchemy.orm import selectinload
+from sqlmodel import  SQLModel, Session, select, create_engine
+from sqlmodel import Field, Relationship
 from fastapi import FastAPI, HTTPException
 from faker import Faker
 from typing import List, Union
 
-#Define model classes
+#Define model classes Customer and Order, and their base classes
+#Base classes are used for input and output validation
+#Model classes are used for database interaction
+#Relationships are defined in the model classes
+
+#A customer can have multiple orders
 
 class OrderBase(BaseModel):
     name: str
-    cost: str
-
-class Order(SQLModel, OrderBase, table = True):
-    id: int = Field(default = None, primary_key = True)
+    cost: Decimal
+    customer_id: int
+    class Config:
+        orm_mode = True
 
 class CustomerBase(BaseModel):
     name: str
-    email: str
     address: str
+    email: str
+    class Config:
+        orm_mode = True
 
-class Customer(SQLModel, CustomerBase, table = True):
-    id: int = Field(default = None, primary_key = True)
+class Customer(CustomerBase, SQLModel, table=True):
+    __tablename__ = "customers"
+    id: int = Field(default=None, primary_key=True)
+    orders: List["Order"] = Relationship(back_populates="customer")
+
+class Order(OrderBase, SQLModel, table=True):
+    __tablename__ = "orders"
+    id: int = Field(default=None, primary_key=True)
+    customer_id: int = Field(default = None, foreign_key="customers.id")
+    customer: Customer = Relationship(back_populates="orders")
+
+class CustomerRead(CustomerBase):
+    orders: List[Order] = []
 
 #Setup database in memory
 sqlite_url = f"sqlite://"
@@ -36,6 +58,15 @@ with Session(engine) as sesh:
         sesh.add(customer)
     sesh.commit()
 
+with Session(engine) as sesh:
+    for customer in sesh.exec(select(Customer)).all():
+        for order in (Order(name = fake.text()
+                            ,cost = fake.pydecimal(2,2)
+                            ,customer_id = customer.id)
+                            for i in range(3)):
+            sesh.add(order)
+    sesh.commit()
+
 #Setup App
 app = FastAPI()
 app.title = "Hello World!"
@@ -43,9 +74,15 @@ app.description = "Carpe Diem"
 
 #Define Routes
 @app.get("/get-customers/", status_code = 200)
-async def get_customers() -> List[Customer]:
+async def get_customers(offset: int = None, limit: int = None) -> List[Customer]:
+    #Use offsets and limits to page through results
     with Session(engine) as sesh:
-        sql = select(Customer)
+        offset = offset or 0
+        limit = limit or 0
+        if limit == 0:
+            sql = select(Customer).offset(offset)
+        else:
+            sql = select(Customer).offset(offset).limit(limit)
         results = sesh.exec(sql).all()
         return results
 
@@ -74,3 +111,50 @@ async def update_customer(customer: Customer) -> Customer:
         sesh.commit()
         customer = sesh.get(Customer, customer.id)
         return customer
+
+@app.get("/get-orders/", status_code = 200)
+async def get_orders() -> List[Order]:
+    with Session(engine) as sesh:
+        sql = select(Order)
+        results = sesh.exec(sql).all()
+        return results
+    
+@app.get("/get-order-by-id/", status_code = 200)
+async def get_order_by_id(id: int) -> Order:
+    with Session(engine) as sesh:
+        order = sesh.get(Order, id)
+        if order is not None:
+            return order
+        else:
+            raise HTTPException(status_code=404, detail = "Order not found")
+        
+@app.post("/create-order/", status_code = 201)
+async def create_order(base: OrderBase) -> Order:
+    new_order = Order(**base.model_dump())
+    with Session(engine) as sesh:
+        sesh.add(new_order)
+        sesh.commit()
+        sesh.refresh(new_order)
+        return new_order
+    
+@app.put("/update-order/", status_code = 200)
+async def update_order(order: Order) -> Order:
+    with Session(engine) as sesh:
+        sesh.merge(order)
+        sesh.commit()
+        order = sesh.get(Order, order.id)
+        return order
+    
+@app.get("/get-customer-orders/", status_code = 200)
+async def get_customer_orders(id: int) -> List[Order]:
+    with Session(engine) as sesh:
+        sql = select(Order).where(Order.customer_id == id)
+        results = sesh.exec(sql).all()
+        return results
+
+@app.get("/get-customer-with-orders/{id}", status_code = 200, response_model=CustomerRead)
+async def get_customer_with_orders(id: int) -> CustomerRead:
+    with Session(engine) as sesh:
+        sql = select(Customer).where(Customer.id == id).options(selectinload(Customer.orders))
+        results = sesh.exec(sql).first()
+        return results
