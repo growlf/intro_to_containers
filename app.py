e306690b758cd3a92de4e233daaf0a1093e7e083
R Shupe <shipshupe@users.noreply.github.com>
2024-01-14 23:21:01 +0000

    add tags to sort routes

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ app.title = "Hello World!"
 app.description = "Carpe Diem"
 
 #Define Routes
-@app.get("/get-customers/", status_code = 200)
+@app.get("/get-customers/", status_code = 200, tags=["customer"])
 async def get_customers(offset: int = None, limit: int = None) -> List[Customer]:
     #Use offsets and limits to page through results
     with Session(engine) as sesh:
@@ -86,7 +86,7 @@ async def get_customers(offset: int = None, limit: int = None) -> List[Customer]
         results = sesh.exec(sql).all()
         return results
 
-@app.get("/get-customer-by-id/", status_code = 200)
+@app.get("/get-customer-by-id/", status_code = 200, tags=["customer"])
 async def get_customer_by_id(id: int) -> Customer:
     with Session(engine) as sesh:
         customer = sesh.get(Customer, id)
@@ -95,7 +95,7 @@ async def get_customer_by_id(id: int) -> Customer:
         else:
             raise HTTPException(status_code=404, detail = "Customer not found")
 
-@app.post("/create-customer/", status_code = 201)
+@app.post("/create-customer/", status_code = 201, tags=["customer"])
 async def create_customer(base: CustomerBase) -> Customer:
     new_customer = Customer(**base.model_dump())
     with Session(engine) as sesh:
@@ -104,7 +104,7 @@ async def create_customer(base: CustomerBase) -> Customer:
         sesh.refresh(new_customer)
         return new_customer
 
-@app.put("/update-customer/", status_code = 200)
+@app.put("/update-customer/", status_code = 200, tags=["customer"])
 async def update_customer(customer: Customer) -> Customer:
     with Session(engine) as sesh:
         sesh.merge(customer)
@@ -112,14 +112,14 @@ async def update_customer(customer: Customer) -> Customer:
         customer = sesh.get(Customer, customer.id)
         return customer
 
-@app.get("/get-orders/", status_code = 200)
+@app.get("/get-orders/", status_code = 200, tags=["order"])
 async def get_orders() -> List[Order]:
     with Session(engine) as sesh:
         sql = select(Order)
         results = sesh.exec(sql).all()
         return results
     
-@app.get("/get-order-by-id/", status_code = 200)
+@app.get("/get-order-by-id/", status_code = 200, tags=["order"])
 async def get_order_by_id(id: int) -> Order:
     with Session(engine) as sesh:
         order = sesh.get(Order, id)
@@ -128,7 +128,7 @@ async def get_order_by_id(id: int) -> Order:
         else:
             raise HTTPException(status_code=404, detail = "Order not found")
         
-@app.post("/create-order/", status_code = 201)
+@app.post("/create-order/", status_code = 201, tags=["order"])
 async def create_order(base: OrderBase) -> Order:
     new_order = Order(**base.model_dump())
     with Session(engine) as sesh:
@@ -137,7 +137,7 @@ async def create_order(base: OrderBase) -> Order:
         sesh.refresh(new_order)
         return new_order
     
-@app.put("/update-order/", status_code = 200)
+@app.put("/update-order/", status_code = 200, tags=["order"])
 async def update_order(order: Order) -> Order:
     with Session(engine) as sesh:
         sesh.merge(order)
@@ -145,14 +145,14 @@ async def update_order(order: Order) -> Order:
         order = sesh.get(Order, order.id)
         return order
     
-@app.get("/get-customer-orders/", status_code = 200)
+@app.get("/get-customer-orders/", status_code = 200, response_model=List[Order], tags=["order"])
 async def get_customer_orders(id: int) -> List[Order]:
     with Session(engine) as sesh:
         sql = select(Order).where(Order.customer_id == id)
         results = sesh.exec(sql).all()
         return results
 
-@app.get("/get-customer-with-orders/{id}", status_code = 200, response_model=CustomerRead)
+@app.get("/get-customer-with-orders/{id}", status_code = 200, response_model=CustomerRead, tags=["customer"])
 async def get_customer_with_orders(id: int) -> CustomerRead:
     with Session(engine) as sesh:
         sql = select(Customer).where(Customer.id == id).options(selectinload(Customer.orders))
